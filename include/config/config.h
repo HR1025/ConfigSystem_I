b82952d2ed08662d559dbbd0c43b3ed3fa95a7ac
HR1025 <1905811497@qq.com>
2021-06-18 09:12:22 -0700

    开发日志1： （1）修改QT本身的cmake设置 （2）移植yamlcpp库，以静态库的形式 （3）给配置系统开了一个头

--- include/config/config.h
+++ include/config/config.h
@@ -1,4 +1,54 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <string>
+#include <stdint.h>
+#include <functional>
+#include <memory>
+namespace cfgstm {
+
+/**
+ * @brief 配置参数虚基类
+ */
+class ConfigVarBase{
+public:
+    using ptr = std::shared_ptr<ConfigVarBase>;
+
+    /**
+     * @brief 构造函数
+     * @param[in] name 配置参数名称
+     * @param[in] description 配置参数描述
+     */
+    ConfigVarBase(const std::string name, const std::string description):
+        m_name(name), m_description(description)
+    {};
+
+    /**
+     * @brief  将类型转化为字符串
+     * @retval 转化完的字符串
+     */
+    virtual std::string toString() = 0;
+
+    /**
+     * @brief 将字符串转化为类型
+     * @retval 是否转化成功
+     */
+    virtual bool fromSring(const std::string& val) = 0;
+
+    /**
+     * @brief  获取类型的字符串名称
+     * @retval 类型的字符串名称
+     */
+    virtual std::string getName() const = 0;
+
+private:
+    std::string m_name;                     // 配置参数的名称
+    std::string m_description;              // 配置参数的描述
+    //std::map<uint64_t, on_change_cb> m_cbs; // 变更回调函数组
+};
+
+
+}
+
+
 #endif // CONFIG_H
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include <iostream>
+#include <yaml-cpp/yaml.h>
+#include <config/config.h>
+using namespace std;
+
+int main()
+{
+    YAML::Node root;
+    cout<<"Hello World!"<<endl;
+
+    return 0;
+}
